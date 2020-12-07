271829451babcaa39a0960b48b9aa9fb3f236d3d
Local Al <alexander.s.woolley@gmail.com>
2020-12-06 18:27:30 -0800

    time displayed and borders added

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
+//target and set current date
+var currentDateDisplay = $("#currentDayContain");
+var dateTimeCode = 
+currentDateDisplay.text(moment())
+
 //target container section of Jumbo for cal block
 var calGrid = $(".container");
 
-//create row
-var row1 = $("<div>");
-row1.addClass("row")
+
